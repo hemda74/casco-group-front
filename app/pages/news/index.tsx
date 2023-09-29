213bf6958d627fa61e25418c7f5106dad2252a32
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-09-29 16:17:07 +0300

    update news

--- app/pages/news/index.tsx
+++ app/pages/news/index.tsx
@@ -1,24 +1,23 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement } from 'react';
 import Head from 'next/head';
-import ViewerLayout from '../../layouts/ViewerLayout';
 import { NextPageWithLayout } from '../_app';
 import News from '../../components/News';
 import OpeningSoon from '../../components/OpeningSoon';
 type Props = {};
 // dymmy data for ui till handle working with api
-const news:NextPageWithLayout = (props:Props) => {
+const news: NextPageWithLayout = (props: Props) => {
   return (
     <>
-    <Head>
+      <Head>
         <title>News | CASCO</title>
-    </Head>
-    <main>
-      {/* <News/> */}
-      <OpeningSoon/>
-    </main>
+      </Head>
+      <main>
+        {/* <News/> */}
+        <OpeningSoon />
+      </main>
     </>
-  )
-}
+  );
+};
 // adding Layout
 // news.getLayout = function getLayout(news: ReactElement) {
 // return <ViewerLayout childern={news}></ViewerLayout>;}
