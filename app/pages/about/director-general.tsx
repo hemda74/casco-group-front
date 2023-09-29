d31562e03d5284804462ca66b2f24968bc086778
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-09-29 15:54:32 +0300

    update deploy errors

--- app/pages/about/director-general.tsx
+++ app/pages/about/director-general.tsx
@@ -1,23 +1,21 @@
-
-import React, { ReactElement } from 'react'
+import React, { ReactElement } from 'react';
 import Head from 'next/head';
-import ViewerLayout from '../../layouts/ViewerLayout6';
 import { NextPageWithLayout } from '../_app';
 import DriectorGeneral from '../../components/DriectorGeneral';
 type Props = {};
 // dymmy data for ui till handle working with api
 
-const directormanger:NextPageWithLayout = (props:Props) => {
+const directormanger = (props: Props) => {
   return (
     <>
-    <Head>
+      <Head>
         <title>CASCO | Director General</title>
-    </Head>
-    <DriectorGeneral/>
+      </Head>
+      <DriectorGeneral />
     </>
-  )
-}
+  );
+};
 // adding Layout
-directormanger.getLayout = function getLayout(directormanger: ReactElement) {
-return <ViewerLayout childern={directormanger}></ViewerLayout>;}
+// directormanger.getLayout = function getLayout(directormanger: ReactElement) {
+// return <ViewerLayout childern={directormanger}></ViewerLayout>;}
 export default directormanger;
