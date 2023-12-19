25ab9ab0c3ce0b3d16926c488a21937e50a6b5be
hemda74 <92821656+hemda74@users.noreply.github.com>
2023-12-19 15:03:17 +0200

    adding new file

--- app/components/about-section/aboutcomp.tsx
+++ app/components/about-section/aboutcomp.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import styles from '../../styles/about.module.css';
 const MainAbout = () => {
   return (
-    <div className="d-felx m-auto ">
-      <span className={styles.mainSpan}> About </span>
-      <div className="container d-flex align-items-center justify-content-center">
+    <div className="d-felx m-auto text-center">
+      <span className={`${styles.mainSpan}`}> About </span>
+      <div className=" col-10 container d-flex align-items-center justify-content-center">
         <div className={`${styles.parent}`}>
           <div
-            className={`${styles.div1} d-flex align-items-center justify-content-center`}>
-            <h1>hello world</h1>{' '}
-          </div>
+            className={`${styles.div1} d-flex align-items-center justify-content-center`}></div>
           <div
             className={`${styles.div2} d-flex align-items-center justify-content-center`}>
             <h1>hello world</h1>{' '}
