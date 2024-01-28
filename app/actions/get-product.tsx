5dd32a8e7f52a6e1cafbfbe91d3ecccb174eeeb5
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2024-01-29 00:26:03 +0200

    adding types & query string

--- app/actions/get-product.tsx
+++ app/actions/get-product.tsx
@@ -1,6 +1,5 @@
-import { Product } from "@/types";
-
-const URL=`${process.env.NEXT_PUBLIC_API_URL}/products`;
+import { Product } from '../types';
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
 const getProduct = async (id: string): Promise<Product> => {
   const res = await fetch(`${URL}/${id}`);
