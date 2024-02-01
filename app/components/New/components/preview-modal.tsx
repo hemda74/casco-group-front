4d8c61aeafea6e18363df68806afd7a4079d893b
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2024-02-02 00:26:12 +0200

    handling next version in components

--- app/components/New/components/preview-modal.tsx
+++ app/components/New/components/preview-modal.tsx
@@ -1,24 +1,20 @@
-"use client";
+'use client';
 
-import usePreviewModal from "@/hooks/use-preview-modal";
-import Gallery from "@/components/gallery";
-import Info from "@/components/info";
-import Modal from "@/components/ui/modal";
-
+import usePreviewModal from '../../../hooks/use-preview-modal';
+import Gallery from '../components/gallery';
+import Info from '../components/info';
+import Modal from '../components/ui/modal';
 
 const PreviewModal = () => {
   const previewModal = usePreviewModal();
-  const product = usePreviewModal((state) => state.data);
+  const product = usePreviewModal(state => state.data);
 
   if (!product) {
     return null;
   }
 
-  return ( 
-    <Modal 
-      open={previewModal.isOpen} 
-      onClose={previewModal.onClose}
-    >
+  return (
+    <Modal open={previewModal.isOpen} onClose={previewModal.onClose}>
       <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
         <div className="sm:col-span-4 lg:col-span-5">
           <Gallery images={product.images} />
@@ -29,6 +25,6 @@ const PreviewModal = () => {
       </div>
     </Modal>
   );
-}
- 
+};
+
 export default PreviewModal;
