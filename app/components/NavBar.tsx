8d40f6f4328774672a2d756fdee82da305fbf5ee
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2024-02-17 22:58:44 +0200

    finish Navbar layout

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -8,8 +8,7 @@ const NavBar = () => {
   return (
     //design nav
     <>
-      <nav
-        className={`navbar navbar-expand-lg sticky-top  ${styles.navBar}`}>
+      <nav className={`navbar navbar-expand-lg sticky-top  ${styles.navBar}`}>
         <div
           className={`container-fluid d-flex justify-content-between align-items-center ms-xl-4 ms-lg-4 ms-md-4 `}>
           <Link href={'/'} className="navbar-brand mb-0 mt-0">
@@ -31,75 +30,67 @@ const NavBar = () => {
             aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarColor01">
-            <ul className="navbar-nav m-auto ">
-              <li className={`nav-item ms-3 ${styles.navItem}`}>
-              </li>
-            </ul>
-            </div>
-            <div className="collapse navbar-collapse" id="navbarColor01">
-            <ul className="navbar-nav m-auto ">
-              <li className={`nav-item ms-3 ${styles.navItem}`}>
-              </li>
-            </ul>
-            </div> <div className="collapse navbar-collapse" id="navbarColor01">
-            <ul className="navbar-nav m-auto ">
-              <li className={`nav-item ms-3 ${styles.navItem}`}>
-              </li>
-            </ul>
-            </div>
-            <div className="collapse navbar-collapse" id="navbarColor01">
-            <ul className="navbar-nav m-auto ">
-              <li className=  {`nav-item ms-3 ${styles.navItem}`}>
-                <Link href={'/courses'} className={`nav-link`} id="link">
-                  <p className={`${styles.navLink}  fw-bold mt-1`} data-trans="CoursesNav">
-                  </p>
-                </Link> 
-              </li>
-              <li className=  {`nav-item ms-3 ${styles.navItem}`}>
-                <Link href={'/about'} className={`nav-link`} id="link">
-                  <p className={`${styles.navLink} fw-bold mt-1`} data-trans="AboutCascoNav">
-                  </p>
-                </Link> 
-              </li>
-              <li className=  {`nav-item ms-3 ${styles.navItem}`}>
-                <Link href={'/services'} className={`nav-link`} id="link">
-                  <p className={`${styles.navLink} fw-bold mt-1`} data-trans="ServicesNav">
-                  </p>
-                </Link> 
-              </li>
-              <li className=  {`nav-item ms-3 ${styles.navItem}`}>
-                <Link href={'/industries'} className={`nav-link`} id="link">
-                  <p className={`${styles.navLink}  fw-bold mt-1`} data-trans="industriesNav">
-                  </p>
-                </Link> 
-              </li>
-              <li className=  {`nav-item ms-3 ${styles.navItem}`}>
-                <Link href={'/news-insights'} className={`nav-link`} id="link">
-                  <p className={`${styles.navLink} fw-bold mt-1`}data-trans="NewsNav">
-                  </p>
-                </Link> 
-              </li>
-              <li className=  {`nav-item ms-3 ${styles.navItem}`}>
-                <Link href={'/news-insights'} className={`nav-link`} id="link">
-                  <p className={`${styles.navLink} fw-bold mt-1`} data-trans="CareersNav">
-                  </p>
-                </Link> 
-              </li> 
-                <li className=  {`nav-item ms-3 ${styles.navItem}`}>
-                <Link href={'/news-insights'} className={`nav-link`} id="link">
-                  <p className={`${styles.navLink} fw-bold mt-1 `} data-trans="ContactUsNav">
-                  </p>
-                </Link> 
-              </li>
-              <li className=  {`nav-item ms-3 ${styles.navItem}`}>
-                <div className={`mt-3`}>
-                <Navbar/>
-                </div>
-              </li>
+        </div>
+        <div
+          className="collapse navbar-collapse ms-md-3 ms-lg-4 ms-xl-4 me-md-3 me-lg-4 me-xl-4"
+          id="navbarColor01">
+          <ul className="navbar-nav m-auto ">
+            <li className={`nav-item ms-3 ${styles.navItem}`}>
+              <Link href={'/courses'} className={`nav-link`} id="link">
+                <p
+                  className={`${styles.navLink}  fw-bold mt-1 text-nowrap`}
+                  data-trans="CoursesNav"></p>
+              </Link>
+            </li>
+            <li className={`nav-item ms-3 ${styles.navItem}`}>
+              <Link href={'/about'} className={`nav-link`} id="link">
+                <p
+                  className={`${styles.navLink} fw-bold mt-1 text-nowrap`}
+                  data-trans="AboutCascoNav"></p>
+              </Link>
+            </li>
+            <li className={`nav-item ms-3 ${styles.navItem}`}>
+              <Link href={'/services'} className={`nav-link`} id="link">
+                <p
+                  className={`${styles.navLink} fw-bold mt-1 text-nowrap`}
+                  data-trans="ServicesNav"></p>
+              </Link>
+            </li>
+            <li className={`nav-item ms-3 ${styles.navItem}`}>
+              <Link href={'/industries'} className={`nav-link`} id="link">
+                <p
+                  className={`${styles.navLink}  fw-bold mt-1 text-nowrap`}
+                  data-trans="industriesNav"></p>
+              </Link>
+            </li>
+            <li className={`nav-item ms-3 ${styles.navItem}`}>
+              <Link href={'/news-insights'} className={`nav-link`} id="link">
+                <p
+                  className={`${styles.navLink} fw-bold mt-1 text-nowrap`}
+                  data-trans="NewsNav"></p>
+              </Link>
+            </li>
+            <li className={`nav-item ms-3 ${styles.navItem}`}>
+              <Link href={'/careers'} className={`nav-link`} id="link">
+                <p
+                  className={`${styles.navLink} fw-bold mt-1 text-nowrap`}
+                  data-trans="CareersNav"></p>
+              </Link>
+            </li>
+            <li className={`nav-item ms-3 ${styles.navItem}`}>
+              <Link href={'/contact-us'} className={`nav-link`} id="link">
+                <p
+                  className={`${styles.navLink} fw-bold mt-1 text-nowrap `}
+                  data-trans="ContactUsNav"></p>
+              </Link>
+            </li>
+            <li className={`nav-item ms-3 ${styles.navItem}`}>
+              <div className={`mt-3`}>
+                <Navbar />
+              </div>
+            </li>
           </ul>
-            </div>
-          </div>
+        </div>
       </nav>
     </>
   );
