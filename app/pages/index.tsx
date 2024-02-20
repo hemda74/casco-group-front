7cf733b9ef28cd26d6130c413ae4aa5dec1eef52
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2024-02-21 00:23:31 +0200

    update globals.css (addding main css file)

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -6,6 +6,10 @@ import ContactUs from '../components/ContactUs';
 import { useLanguage } from '../Context/LanguageContext';
 import Layout from '../components/Layout';
 import About from '../components/About';
+import { Cairo } from 'next/font/google';
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Cairo({ subsets: ['latin'] });
 type Props = {};
 const Contactus: NextPageWithLayout = (props: Props) => {
   const { language } = useLanguage();
