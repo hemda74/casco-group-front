ddb14575dc5c3b9893cefc04acb1e2e8227fc92f
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2024-02-25 00:12:47 +0200

    handle bulid errors

--- app/pages/contact-us.tsx
+++ app/pages/contact-us.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useEffect, useState } from 'react';
+import React, { ReactElement } from 'react';
 import Head from 'next/head';
 import ViewerLayout from '../layouts/ViewerLayout';
 import { NextPageWithLayout } from './_app';
