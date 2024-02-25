b13dfaf2d3668d40dbf8b20155c08f867500f167
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2024-02-26 01:47:59 +0200

    adding new team member page

--- app/components/About/MeetOurTeamEn.tsx
+++ app/components/About/MeetOurTeamEn.tsx
@@ -9,27 +9,19 @@ const MeetOurTeamEn = () => {
         <div className="container card-grid">
           <div className="row">
             <div className="col-12 text-center">
-              <h2 className="title title--lg red-1-color">
-                Meet the
-                <span className="js-team-title"></span>
-                team
-              </h2>
+              <h2 className="title title--lg red-1-color">Meet The Team</h2>
             </div>
           </div>
           <div className="row js-team-top-level">
             <div className="col-xl-4 col-md-6 order-1">
-              <div
-                className="card card--split red-2-background"
-                data-city-id="9">
+              <div className="card card--split red-1-background">
                 <div className="row no-gutters">
                   <div className="col-6">
                     <div className="card__content">
-                      <div className="title title--sm" data-title="Paris">
-                        Paris
-                      </div>
+                      <div className="title title--sm">Egypt</div>
 
                       <div className="card--split__hover title--medium-weight">
-                        Meet the leadership team
+                        Meet The Team
                       </div>
                     </div>
                   </div>
@@ -43,20 +35,14 @@ const MeetOurTeamEn = () => {
               </div>
             </div>
             <div className="col-xl-4 col-md-6 order-2">
-              <div
-                className="card card--split red-3-background"
-                data-city-id="10">
+              <div className="card card--split red-1-background">
                 <div className="row no-gutters">
                   <div className="col-6">
                     <div className="card__content">
-                      <div
-                        className="title title--sm"
-                        data-title="London leadership">
-                        London
-                      </div>
+                      <div className="title title--sm">UAE</div>
 
                       <div className="card--split__hover title--medium-weight">
-                        Meet the leadership team
+                        Meet the Team
                       </div>
                     </div>
                   </div>
@@ -70,17 +56,11 @@ const MeetOurTeamEn = () => {
               </div>
             </div>
             <div className="col-xl-4 col-md-6 order-4">
-              <div
-                className="card card--split red-4-background"
-                data-city-id="223">
+              <div className="card card--split red-1-background">
                 <div className="row no-gutters">
                   <div className="col-6">
                     <div className="card__content">
-                      <div
-                        className="title title--sm"
-                        data-title="Central Europe">
-                        Amsterdam
-                      </div>
+                      <div className="title title--sm">KSA</div>
 
                       <div className="card--split__hover title--medium-weight">
                         Meet the team
@@ -92,335 +72,6 @@ const MeetOurTeamEn = () => {
                     style={{
                       backgroundImage:
                         'url(https://www.argonandco.com/wp-content/uploads/2021/07/IMG_0128-683x1024.jpg)',
-                    }}></div>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 order-8">
-              <div
-                className="card card--split red-5-background"
-                data-city-id="223">
-                <div className="row no-gutters">
-                  <div className="col-6">
-                    <div className="card__content">
-                      <div
-                        className="title title--sm"
-                        data-title="Central Europe">
-                        Dusseldorf
-                      </div>
-
-                      <div className="card--split__hover title--medium-weight">
-                        Meet the team
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    className="col-6 card--split__image grayscale"
-                    style={{
-                      backgroundImage:
-                        'url(https://www.argonandco.com/wp-content/uploads/2021/07/L.-Vialatte-20140805-894x1024.jpg)',
-                    }}></div>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 order-9">
-              <div
-                className="card card--split red-1-background"
-                data-city-id="223">
-                <div className="row no-gutters">
-                  <div className="col-6">
-                    <div className="card__content">
-                      <div
-                        className="title title--sm"
-                        data-title="Central Europe">
-                        Lausanne
-                      </div>
-
-                      <div className="card--split__hover title--medium-weight">
-                        Meet the team
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    className="col-6 card--split__image grayscale"
-                    style={{
-                      backgroundImage:
-                        'url(https://www.argonandco.com/wp-content/uploads/2021/07/V.-Trandafir-20140805-894x1024.jpg)',
-                    }}></div>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 order-3">
-              <div
-                className="card card--split red-2-background"
-                data-city-id="16">
-                <div className="row no-gutters">
-                  <div className="col-6">
-                    <div className="card__content">
-                      <div className="title title--sm" data-title="Abu Dhabi">
-                        Abu Dhabi
-                      </div>
-
-                      <div className="card--split__hover title--medium-weight">
-                        Meet the team
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    className="col-6 card--split__image grayscale"
-                    style={{
-                      backgroundImage:
-                        'url(https://www.argonandco.com/wp-content/uploads/2024/01/Jihen.jpg)',
-                    }}></div>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 order-12">
-              <div
-                className="card card--split red-3-background"
-                data-city-id="237">
-                <div className="row no-gutters">
-                  <div className="col-6">
-                    <div className="card__content">
-                      <div className="title title--sm" data-title="Riyadh">
-                        Riyadh
-                      </div>
-
-                      <div className="card--split__hover title--medium-weight">
-                        Meet the team
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    className="col-6 card--split__image grayscale"
-                    style={{
-                      backgroundImage:
-                        'url(https://www.argonandco.com/wp-content/uploads/2023/08/IMG_8452-scaled-e1692087641232-1024x971.jpg)',
-                    }}></div>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 order-5">
-              <div
-                className="card card--split red-4-background"
-                data-city-id="15">
-                <div className="row no-gutters">
-                  <div className="col-6">
-                    <div className="card__content">
-                      <div className="title title--sm" data-title="Atlanta">
-                        Atlanta
-                      </div>
-
-                      <div className="card--split__hover title--medium-weight">
-                        Meet the team
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    className="col-6 card--split__image grayscale"
-                    style={{
-                      backgroundImage:
-                        'url(https://www.argonandco.com/wp-content/uploads/2020/01/Atlanta.jpg)',
-                    }}></div>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 order-7">
-              <div
-                className="card card--split red-5-background"
-                data-city-id="217">
-                <div className="row no-gutters">
-                  <div className="col-6">
-                    <div className="card__content">
-                      <div className="title title--sm" data-title="Chicago">
-                        Chicago
-                      </div>
-
-                      <div className="card--split__hover title--medium-weight">
-                        Meet the team
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    className="col-6 card--split__image grayscale"
-                    style={{
-                      backgroundImage:
-                        'url(https://www.argonandco.com/wp-content/uploads/2021/04/TTaylor2-scaled-e1617698012197-1024x850.jpg)',
-                    }}></div>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 order-12">
-              <div
-                className="card card--split red-1-background"
-                data-city-id="266">
-                <div className="row no-gutters">
-                  <div className="col-6">
-                    <div className="card__content">
-                      <div className="title title--sm" data-title="São Paulo">
-                        São Paulo
-                      </div>
-
-                      <div className="card--split__hover title--medium-weight">
-                        Meet the team
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    className="col-6 card--split__image grayscale"
-                    style={{
-                      backgroundImage:
-                        'url(https://www.argonandco.com/wp-content/uploads/2022/11/Eduardo-candid-photo-for-Sao-Paulo-office-1024x682.jpg)',
-                    }}></div>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 order-6">
-              <div
-                className="card card--split red-2-background"
-                data-city-id="214">
-                <div className="row no-gutters">
-                  <div className="col-6">
-                    <div className="card__content">
-                      <div className="title title--sm" data-title="Auckland">
-                        Auckland
-                      </div>
-
-                      <div className="card--split__hover title--medium-weight">
-                        Meet the team
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    className="col-6 card--split__image grayscale"
-                    style={{
-                      backgroundImage:
-                        'url(https://www.argonandco.com/wp-content/uploads/2022/08/Natalie-Russell-photo123.jpg)',
-                    }}></div>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 order-10">
-              <div
-                className="card card--split red-3-background"
-                data-city-id="19">
-                <div className="row no-gutters">
-                  <div className="col-6">
-                    <div className="card__content">
-                      <div className="title title--sm" data-title="Melbourne">
-                        Melbourne
-                      </div>
-
-                      <div className="card--split__hover title--medium-weight">
-                        Meet the team
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    className="col-6 card--split__image grayscale"
-                    style={{
-                      backgroundImage:
-                        'url(https://www.argonandco.com/wp-content/uploads/2023/04/Jai-Copy-scaled-e1682294511351-1024x935.jpeg)',
-                    }}></div>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 order-14">
-              <div
-                className="card card--split red-4-background"
-                data-city-id="20">
-                <div className="row no-gutters">
-                  <div className="col-6">
-                    <div className="card__content">
-                      <div className="title title--sm" data-title="Sydney">
-                        Sydney
-                      </div>
-
-                      <div className="card--split__hover title--medium-weight">
-                        Meet the team
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    className="col-6 card--split__image grayscale"
-                    style={{
-                      backgroundImage:
-                        'url(https://www.argonandco.com/wp-content/uploads/2022/04/image002-e1649946837463.png)',
-                    }}></div>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 order-8">
-              <div
-                className="card card--split red-5-background"
-                data-city-id="253">
-                <div className="row no-gutters">
-                  <div className="col-6">
-                    <div className="card__content">
-                      <div className="title title--sm" data-title="Hong Kong">
-                        Hong Kong
-                      </div>
-
-                      <div className="card--split__hover title--medium-weight">
-                        Meet the team
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    className="col-6 card--split__image grayscale"
-                    style={{
-                      backgroundImage:
-                        'url(https://www.argonandco.com/wp-content/uploads/2024/02/DSC06232-1024x683.jpg)',
-                    }}></div>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 order-11">
-              <div
-                className="card card--split red-1-background"
-                data-city-id="17">
-                <div className="row no-gutters">
-                  <div className="col-6">
-                    <div className="card__content">
-                      <div className="title title--sm" data-title="Mumbai">
-                        Mumbai
-                      </div>
-
-                      <div className="card--split__hover title--medium-weight">
-                        Meet the team
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    className="col-6 card--split__image grayscale"
-                    style={{
-                      backgroundImage:
-                        'url(https://www.argonandco.com/wp-content/uploads/2019/12/Screenshot-2019-12-17-at-15.07.12-e1576595751558-760x1024.png)',
-                    }}></div>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-6 order-13">
-              <div
-                className="card card--split red-2-background"
-                data-city-id="18">
-                <div className="row no-gutters">
-                  <div className="col-6">
-                    <div className="card__content">
-                      <div className="title title--sm" data-title="Singapore">
-                        Singapore
-                      </div>
-
-                      <div className="card--split__hover title--medium-weight">
-                        Meet the team
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    className="col-6 card--split__image grayscale"
-                    style={{
-                      backgroundImage:
-                        'url(https://www.argonandco.com/wp-content/uploads/2023/12/00100lrPORTRAIT_00100_BURST20200904174226195_COVER2-981x1024.jpg)',
                     }}></div>
                 </div>
               </div>
