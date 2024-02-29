0bc771f06950bb2dbc5effe4c92d34ce8c2f6aa9
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2024-02-29 03:08:55 +0200

    update contact us

--- app/pages/contact-us.tsx
+++ app/pages/contact-us.tsx
@@ -6,6 +6,7 @@ import ContactUs from '../components/ContactUs';
 import { useLanguage } from '../Context/LanguageContext';
 import Layout from '../components/Layout';
 import styles from '../styles/Main.module.css';
+import StackHolders from '../components/test';
 
 type Props = {};
 // dymmy data for ui till handle working with api
@@ -19,7 +20,7 @@ const Contactus: NextPageWithLayout = (props: Props) => {
       <Layout>
         {language === 'en' ? (
           <main className={`${styles.bodyContainer}`}>
-            <ContactUs />
+            <StackHolders />
           </main>
         ) : (
           <main className={`${styles.bodyContainer}`}></main>
