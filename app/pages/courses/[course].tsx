3c689e236c9564c5a77f6e97186ca0c4823a4e92
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2024-03-05 23:16:14 +0200

    adding single course page

---
+++ app/pages/courses/[course].tsx
@@ -0,0 +1,35 @@
+import React, { ReactElement } from 'react';
+import Head from 'next/head';
+import { useLanguage } from '../../Context/LanguageContext';
+import Layout from '../../components/Layout';
+import styles from '../../styles/Main.module.css';
+import OldNavBar from '../../components/OldNavBar';
+import Footer from '../../components/Footer';
+import FooterAr from '../../components/FooterAr';
+type Props = {};
+// dymmy data for ui till handle working with api
+const Contactus = (props: Props) => {
+  const { language } = useLanguage();
+  return (
+    <>
+      <Head>
+        <title>Courses | CASCO</title>
+      </Head>
+      <Layout>
+        {language === 'en' ? (
+          <main className={`${styles.bodyContainer}`}>
+            <OldNavBar />
+            <Footer />
+          </main>
+        ) : (
+          <main className={`${styles.bodyContainer}`}>
+            <OldNavBar />
+            <FooterAr />
+          </main>
+        )}
+      </Layout>
+    </>
+  );
+};
+
+export default Contactus;
