8a12c0f3bbfc08084a4a7ed13955cd3658815e76
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2024-03-11 00:45:44 +0200

    Adding Operations strategy Section In Services

---
+++ app/components/Services/OperationsStrategyEn.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OperationsStrategyEn = () => {
+  return <div></div>;
+};
+
+export default OperationsStrategyEn;
---
+++ app/pages/services/operations-strategy/index.tsx
@@ -0,0 +1,37 @@
+import React, { ReactElement } from 'react';
+import Head from 'next/head';
+import { useLanguage } from '../../../Context/LanguageContext';
+import Layout from '../../../components/Layout';
+import styles from '../../../styles/Main.module.css';
+import MainPageEn from '../../../components/Services/MainPageEn';
+import MainPageAr from '../../../components/Services/MainPageAr';
+import { NextPageWithLayout } from '../../_app';
+import ViewerLayout from '../../../layouts/ViewerLayout';
+type Props = {};
+// dymmy data for ui till handle working with api
+const Contactus: NextPageWithLayout = (props: Props) => {
+  const { language } = useLanguage();
+  return (
+    <>
+      <Head>
+        <title>Services | CASCO</title>
+      </Head>
+      <Layout>
+        {language === 'en' ? (
+          <main className={`${styles.bodyContainer}`}>
+            <MainPageEn />
+          </main>
+        ) : (
+          <main className={`${styles.bodyContainer}`}>
+            <MainPageAr />
+          </main>
+        )}
+      </Layout>
+    </>
+  );
+};
+// adding Layout
+Contactus.getLayout = function getLayout(contactus: ReactElement) {
+  return <ViewerLayout childern={contactus}></ViewerLayout>;
+};
+export default Contactus;
