0350ea9373aa0a5438898a6453a20332d5905013
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2024-03-13 05:22:54 +0200

    change recogention slider running mechanism

--- app/components/About/Recogention.js
+++ app/components/About/Recogention.js
@@ -3,25 +3,19 @@ import Slider from 'react-slick';
 import styles from '../../styles/AccreditedBodiesCompaines.module.css';
 export default class SwipeToSlide extends Component {
   render() {
-    const settings = {
-      className: 'center',
-      infinite: true,
-      centerPadding: '50px',
-      swipeToSlide: true,
+    var settings = {
       dots: false,
+      infinite: true,
+      speed: 500,
       slidesToShow: 8,
-      speed: 10000,
-      slidesToScroll: 2,
-      autoplay: true,
-      autoplaySpeed: 0,
-      pauseOnHover: true,
-      lazyLoad: true,
+      slidesToScroll: 1,
+      initialSlide: 0,
       responsive: [
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 5,
-            slidesToScroll: 2,
+            slidesToShow: 4,
+            slidesToScroll: 1,
             infinite: true,
             dots: false,
           },
@@ -29,26 +23,23 @@ export default class SwipeToSlide extends Component {
         {
           breakpoint: 600,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
+            slidesToShow: 3,
+            slidesToScroll: 1,
             initialSlide: 2,
-            dots: false,
           },
         },
         {
           breakpoint: 480,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2,
-            dots: false,
+            slidesToScroll: 1,
           },
         },
       ],
     };
     return (
-      <section className="pad-100-vert off-screen off-screen--hide off-screen--fade-up">
-        <div className="container mb-5">
+      <section className="pad-80  off-screen off-screen--hide off-screen--fade-up">
+        <div className="container">
           <div className="row justify-content-center">
             <div className="col-md-8 text-center">
               <h2 className="title title--lg red-1-color color-red-1">
@@ -62,113 +53,220 @@ export default class SwipeToSlide extends Component {
             </div>
           </div>
         </div>
-        <Slider {...settings} className="mt-5">
-          <div className={`${styles.bigDiv}`}>
-            <img
-              className={`${styles.imageDiv}`}
-              src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.14.png"
-              alt=" "
-            />
-          </div>
-          <div className={`${styles.bigDiv}`}>
-            <img
-              className={`${styles.imageDiv}`}
-              src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.34.png"
-              alt=" "
-            />
-          </div>
-          <div className={`${styles.bigDiv}`}>
-            <img
-              className={`${styles.imageDiv}`}
-              src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.14.png"
-              alt=" "
-            />
-          </div>
-          <div className={`${styles.bigDiv}`}>
-            <img
-              className={`${styles.imageDiv}`}
-              src="https://www.argonandco.com/wp-content/uploads/2020/01/sig-custom-gartner-logo.jpg.imgo_.jpg"
-              alt=" "
-            />
-          </div>
-          <div className={`${styles.bigDiv}`}>
-            <img
-              className={`${styles.imageDiv}`}
-              src="https://www.argonandco.com/wp-content/uploads/2019/12/Great-places-to-work.png"
-              alt=" "
-            />
-          </div>{' '}
-          <div className={`${styles.bigDiv}`}>
-            <img
-              className={`${styles.imageDiv}`}
-              src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.14.png"
-              alt=" "
-            />
-          </div>
-          <div className={`${styles.bigDiv}`}>
-            <img
-              className={`${styles.imageDiv}`}
-              src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.34.png"
-              alt=" "
-            />
-          </div>
-          <div className={`${styles.bigDiv}`}>
-            <img
-              className={`${styles.imageDiv}`}
-              src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.14.png"
-              alt=" "
-            />
-          </div>
-          <div className={`${styles.bigDiv}`}>
-            <img
-              className={`${styles.imageDiv}`}
-              src="https://www.argonandco.com/wp-content/uploads/2020/01/sig-custom-gartner-logo.jpg.imgo_.jpg"
-              alt=" "
-            />
-          </div>
-          <div className={`${styles.bigDiv}`}>
-            <img
-              className={`${styles.imageDiv}`}
-              src="https://www.argonandco.com/wp-content/uploads/2019/12/Great-places-to-work.png"
-              alt=" "
-            />
-          </div>{' '}
-          <div className={`${styles.bigDiv}`}>
-            <img
-              className={`${styles.imageDiv}`}
-              src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.14.png"
-              alt=" "
-            />
-          </div>
-          <div className={`${styles.bigDiv}`}>
-            <img
-              className={`${styles.imageDiv}`}
-              src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.34.png"
-              alt=" "
-            />
-          </div>
-          <div className={`${styles.bigDiv}`}>
-            <img
-              className={`${styles.imageDiv}`}
-              src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.14.png"
-              alt=" "
-            />
-          </div>
-          <div className={`${styles.bigDiv}`}>
-            <img
-              className={`${styles.imageDiv}`}
-              src="https://www.argonandco.com/wp-content/uploads/2020/01/sig-custom-gartner-logo.jpg.imgo_.jpg"
-              alt=" "
-            />
-          </div>
-          <div className={`${styles.bigDiv}`}>
-            <img
-              className={`${styles.imageDiv}`}
-              src="https://www.argonandco.com/wp-content/uploads/2019/12/Great-places-to-work.png"
-              alt=" "
-            />
-          </div>
-        </Slider>
+        <div className="arrow-6-slider slick-initialized slick-slider">
+          <Slider {...settings} className="mt-5">
+            <div className="slick-slide slick-cloned">
+              <div
+                className={``}
+                style={{ width: '100%', display: 'inline-block' }}>
+                <img
+                  className={``}
+                  src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.14.png"
+                  alt=" "
+                />
+                <p className="fw-semibold">
+                  UK's leading management consultants 2023
+                </p>
+              </div>
+            </div>
+            <div className="slick-slide slick-cloned">
+              <div
+                className={``}
+                style={{ width: '100%', display: 'inline-block' }}>
+                <img
+                  className={``}
+                  src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.34.png"
+                  alt=" "
+                />
+                <p className="fw-semibold">
+                  UK's leading management consultants 2023
+                </p>
+              </div>
+            </div>
+            <div className="slick-slide slick-cloned">
+              <div
+                className={``}
+                style={{ width: '100%', display: 'inline-block' }}>
+                <img
+                  className={``}
+                  src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.14.png"
+                  alt=" "
+                />
+                <p className="fw-semibold">
+                  UK's leading management consultants 2023
+                </p>
+              </div>
+            </div>
+            <div className="slick-slide slick-cloned">
+              <div
+                className={``}
+                style={{ width: '100%', display: 'inline-block' }}>
+                <img
+                  className={``}
+                  src="https://www.argonandco.com/wp-content/uploads/2020/01/sig-custom-gartner-logo.jpg.imgo_.jpg"
+                  alt=" "
+                />
+                <p className="fw-semibold">
+                  UK's leading management consultants 2023
+                </p>
+              </div>
+            </div>
+            <div className="slick-slide slick-cloned">
+              <div
+                className={``}
+                style={{ width: '100%', display: 'inline-block' }}>
+                <img
+                  className={``}
+                  src="https://www.argonandco.com/wp-content/uploads/2019/12/Great-places-to-work.png"
+                  alt=" "
+                />
+                <p className="fw-semibold">
+                  UK's leading management consultants 2023
+                </p>
+              </div>{' '}
+            </div>
+            <div className="slick-slide slick-cloned">
+              <div
+                className={``}
+                style={{ width: '100%', display: 'inline-block' }}>
+                <img
+                  className={``}
+                  src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.14.png"
+                  alt=" "
+                />
+                <p className="fw-semibold">
+                  UK's leading management consultants 2023
+                </p>
+              </div>
+            </div>
+            <div className="slick-slide slick-cloned">
+              <div
+                className={``}
+                style={{ width: '100%', display: 'inline-block' }}>
+                <img
+                  className={``}
+                  src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.34.png"
+                  alt=" "
+                />
+                <p className="fw-semibold">
+                  UK's leading management consultants 2023
+                </p>
+              </div>
+            </div>
+            <div className="slick-slide slick-cloned">
+              <div
+                className={``}
+                style={{ width: '100%', display: 'inline-block' }}>
+                <img
+                  className={``}
+                  src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.14.png"
+                  alt=" "
+                />
+                <p className="fw-semibold">
+                  UK's leading management consultants 2023
+                </p>
+              </div>
+            </div>
+            <div className="slick-slide slick-cloned">
+              <div
+                className={``}
+                style={{ width: '100%', display: 'inline-block' }}>
+                <img
+                  className={``}
+                  src="https://www.argonandco.com/wp-content/uploads/2020/01/sig-custom-gartner-logo.jpg.imgo_.jpg"
+                  alt=" "
+                />
+                <p className="fw-semibold">
+                  UK's leading management consultants 2023
+                </p>
+              </div>
+            </div>
+            <div className="slick-slide slick-cloned">
+              <div
+                className={``}
+                style={{ width: '100%', display: 'inline-block' }}>
+                <img
+                  className={``}
+                  src="https://www.argonandco.com/wp-content/uploads/2019/12/Great-places-to-work.png"
+                  alt=" "
+                />
+                <p className="fw-semibold">
+                  UK's leading management consultants 2023
+                </p>
+              </div>{' '}
+            </div>
+            <div className="slick-slide slick-cloned">
+              <div
+                className={``}
+                style={{ width: '100%', display: 'inline-block' }}>
+                <img
+                  className={``}
+                  src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.14.png"
+                  alt=" "
+                />
+                <p className="fw-semibold">
+                  UK's leading management consultants 2023
+                </p>
+              </div>
+            </div>
+            <div className="slick-slide slick-cloned">
+              <div
+                className={``}
+                style={{ width: '100%', display: 'inline-block' }}>
+                <img
+                  className={``}
+                  src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.34.png"
+                  alt=" "
+                />
+                <p className="fw-semibold">
+                  UK's leading management consultants 2023
+                </p>
+              </div>
+            </div>
+            <div className="slick-slide slick-cloned">
+              <div
+                className={``}
+                style={{ width: '100%', display: 'inline-block' }}>
+                <img
+                  className={``}
+                  src="https://www.argonandco.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-11-at-14.59.14.png"
+                  alt=" "
+                />
+                <p className="fw-semibold">
+                  UK's leading management consultants 2023
+                </p>
+              </div>
+            </div>
+            <div className="slick-slide slick-cloned">
+              <div
+                className={``}
+                style={{ width: '100%', display: 'inline-block' }}>
+                <img
+                  className={``}
+                  src="https://www.argonandco.com/wp-content/uploads/2020/01/sig-custom-gartner-logo.jpg.imgo_.jpg"
+                  alt=" "
+                />
+                <p className="fw-semibold">
+                  UK's leading management consultants 2023
+                </p>
+              </div>
+            </div>
+            <div className="slick-slide slick-cloned">
+              <div
+                className={``}
+                style={{ width: '100%', display: 'inline-block' }}>
+                <img
+                  className={``}
+                  src="https://www.argonandco.com/wp-content/uploads/2019/12/Great-places-to-work.png"
+                  alt=" "
+                />
+                <p className="fw-semibold">
+                  UK's leading management consultants 2023
+                </p>
+              </div>
+            </div>
+          </Slider>
+        </div>
       </section>
     );
   }
