3f877405bfd430fdbdea53c2ac1e0e2315da591e
Ahmed Hemdan <ahmedashrafhemdan74@gmail.com>
2024-06-10 23:57:31 +0300

    updating the companies section files

--- app/components/HomePage/Companies-Section-En.tsx
+++ app/components/HomePage/Companies-Section-En.tsx
@@ -1,149 +1,51 @@
 import React from 'react';
-const CompaniesSectionEn = () => {
+
+const clientLogos: string[] = [
+  './images/clients/Picture1.jpg',
+  './images/clients/Picture1.jpg',
+  './images/clients/Picture2.png',
+  './images/clients/Picture1.jpg',
+  './images/clients/Picture1.jpg',
+  './images/clients/Picture2.png',
+  './images/clients/Picture3.png',
+  './images/clients/Picture4.png',
+  './images/clients/Picture5.png',
+  './images/clients/Picture6.jpg',
+  './images/clients/Picture7.jpg',
+  './images/clients/Picture8.png',
+  './images/clients/Picture9.png',
+  './images/clients/Picture10.jpg',
+  './images/clients/Picture11.png',
+  './images/clients/Picture12.png',
+  './images/clients/Picture13.jpg',
+  './images/clients/Picture14.png',
+];
+
+const CompaniesSectionEn: React.FC = () => {
+  // Helper function to render client logo divs
+  const renderClientLogos = () => {
+    return clientLogos.map((logo, index) => (
+      <div className="col-6 col-md-3" key={index}>
+        <div className="floating-logo" style={{ backgroundImage: `url(${logo})` }}></div>
+      </div>
+    ));
+  };
+
   return (
-    <>
-      <section className="static-background">
-        <div className="d-flex align-items-center text-center">
-          <div
-            className="background-parallax-logos d-flex flex-column align-items-center"
-            data-scroll-speed="8"
-            style={{ transform: 'translateY(-277.4px)' }}>
-            <div className="row mb-auto">
-              <div className="col-6 col-md-3">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: 'url(./images/clients/Picture1.jpg)',
-                  }}></div>
-              </div>
-              <div className="col-6 col-md-3">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: 'url(./images/clients/Picture1.jpg)',
-                  }}></div>
-              </div>
-              <div className="col-6 col-md-3">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: ' url(./images/clients/Picture2.png)',
-                  }}></div>
-              </div>
-              <div className="col-6 col-md-3">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: 'url(./images/clients/Picture1.jpg)',
-                  }}></div>
-              </div>
-              <div className="col-6 col-md">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: 'url(./images/clients/Picture1.jpg)',
-                  }}></div>
-              </div>
-              <div className="col-6 col-md">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: 'url(./images/clients/Picture2.png)',
-                  }}></div>
-              </div>
-              <div className="col-6 col-md">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: 'url(./images/clients/Picture3.png)',
-                  }}></div>
-              </div>
-              <div className="col-6 col-md">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: 'url(./images/clients/Picture4.png)',
-                  }}></div>
-              </div>
-              <div className="col-6 col-md">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: 'url(./images/clients/Picture5.png)',
-                  }}></div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-6 col-md">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: 'url(./images/clients/Picture6.jpg)',
-                  }}></div>
-              </div>
-              <div className="col-6 col-md">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: 'url(./images/clients/Picture7.jpg)',
-                  }}></div>
-              </div>
-              <div className="col-6 col-md">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: 'url(./images/clients/Picture8.png)',
-                  }}></div>
-              </div>
-              <div className="col-6 col-md">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: 'url(./images/clients/Picture9.png)',
-                  }}></div>
-              </div>
-              <div className="col-6 col-md">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: 'url(./images/clients/Picture10.jpg)',
-                  }}></div>
-              </div>
-              <div className="col-12"></div>
-              <div className="col-6 col-md-3">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: 'url(./images/clients/Picture11.png)',
-                  }}></div>
-              </div>
-              <div className="col-6 col-md-3">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: 'url(./images/clients/Picture12.png)',
-                  }}></div>
-              </div>
-              <div className="col-6 col-md-3">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: 'url(./images/clients/Picture13.jpg)',
-                  }}></div>
-              </div>
-              <div className="col-6 col-md-3">
-                <div
-                  className="floating-logo"
-                  style={{
-                    backgroundImage: 'url(./images/clients/Picture14.png)',
-                  }}></div>
-              </div>
-            </div>
+    <section className="static-background">
+      <div className="d-flex align-items-center text-center">
+        <div
+          className="background-parallax-logos d-flex flex-column align-items-center"
+          data-scroll-speed="8"
+          style={{ transform: 'translateY(-277.4px)' }}
+        >
+          <div className="row mb-auto">
+            {renderClientLogos()}
           </div>
-          <h2 className="title title--lg">We support global clients</h2>
         </div>
-      </section>
-    </>
+        <h2 className="title title--lg">We support global clients</h2>
+      </div>
+    </section>
   );
 };
 
--- app/components/HomePage/FourthSectionMainAr.tsx
+++ app/components/HomePage/FourthSectionMainAr.tsx
@@ -1,162 +1,61 @@
 import Link from 'next/link';
 import React from 'react';
 
-const FourthSectionMainAr = () => {
+interface Service {
+  href: string;
+  title: string;
+}
+
+const services: Service[] = [
+  { href: '/services/sustainability/', title: 'الاستدامة' },
+  { href: '/services/operational-performance/', title: 'الاداء العملي' },
+  { href: '/services/new-products/', title: 'المنتجات الجديدة' },
+  { href: '/services/procurement/', title: 'التحصيل' },
+  { href: '/services/supply-chain-planning/', title: 'تخطيط سلسلة الإمداد' },
+  { href: '/services/manufacturing/', title: 'التصنيع' },
+  { href: '/services/logistics/', title: 'الخدمات اللوجستية' },
+  { href: '/services/channels-to-market/', title: 'قنوات التسويق' },
+  { href: '/services/maintenance-and-after-sales/', title: 'صيانة و بعد البيع' },
+  { href: '/services/transformation-management/', title: 'إدارة التحول' },
+  { href: '/services/digital-and-it/', title: 'تكنولوجيا المعلومات' },
+  { href: '/services/finance/', title: 'الموارد المالية' },
+  { href: '/services/sga/', title: 'SG&A' },
+  { href: '/services/hr/', title: 'الموارد البشرية' },
+];
+
+const FourthSectionMainAr: React.FC = () => {
   return (
-    <>
-      <section
-        dir="rtl"
-        className="strip-padding-lg white-color banner--full-overlay banner red-1-background"
-        style={{
-          backgroundImage:
-            'url(https://www.argonandco.com/wp-content/uploads/2019/10/Compass-edited-e1570192043957.jpg',
-        }}>
-        <div className="container">
-          <div className="row justify-content-center text-center">
-            <div className="col-md-12 off-screen off-screen--hide off-screen--rotate-up">
-              <h2 className="title title--lg">
-                المساعدة في التحديات التشغيلية
-              </h2>
-            </div>
-            <div className="col-lg-7 col-md-9 off-screen off-screen--hide off-screen--rotate-up">
-              <p className="intro">
-                نحن نحفر بعمق لتحديد جذور المشاكل المعقدة والتركيز عليها على
-                تحقيق نتائج ملموسة للعملاء، وهذه هي اختيار تحديات العمليات التي
-                نعمل عليها بانتظام.
-              </p>
-            </div>
+    <section
+      dir="rtl"
+      className="strip-padding-lg white-color banner--full-overlay banner red-1-background"
+      style={{
+        backgroundImage:
+          'url(https://www.argonandco.com/wp-content/uploads/2019/10/Compass-edited-e1570192043957.jpg)',
+      }}
+    >
+      <div className="container">
+        <div className="row justify-content-center text-center">
+          <div className="col-md-12 off-screen off-screen--hide off-screen--rotate-up">
+            <h2 className="title title--lg">المساعدة في التحديات التشغيلية</h2>
           </div>
-          <div className="row link-list off-screen off-screen--hide off-screen--link-list">
-            <div className="col-md-6 col-lg-4">
-              <Link
-                href="/services/sustainability/"
-                className="d-flex align-items-center white-color">
-                <div className="title title--md title title--medium-weight">
-                  الاستدامة
-                </div>
-              </Link>
-            </div>
-            <div className="col-md-6 col-lg-4">
-              <Link
-                href="/services/operational-performance/"
-                className="d-flex align-items-center white-color">
-                <div className="title title--md title title--medium-weight">
-                  الاداء العملي
-                </div>
-              </Link>
-            </div>
-            <div className="col-md-6 col-lg-4">
-              <Link
-                href="/services/new-products/"
-                className="d-flex align-items-center white-color">
-                <div className="title title--md title title--medium-weight">
-                  المنتجات الجديدة{' '}
-                </div>
-              </Link>
-            </div>
-            <div className="col-md-6 col-lg-4">
-              <Link
-                href="/services/procurement/"
-                className="d-flex align-items-center white-color">
-                <div className="title title--md title title--medium-weight">
-                  التحصيل
-                </div>
-              </Link>
-            </div>
-            <div className="col-md-6 col-lg-4">
-              <Link
-                href="/services/supply-chain-planning/"
-                className="d-flex align-items-center white-color">
-                <div className="title title--md title title--medium-weight">
-                  تخطيط سلسلة الإمداد
-                </div>
-              </Link>
-            </div>
-            <div className="col-md-6 col-lg-4">
-              <Link
-                href="/services/manufacturing/"
-                className="d-flex align-items-center white-color">
-                <div className="title title--md title title--medium-weight">
-                  التصنيع
-                </div>
-              </Link>
-            </div>
-            <div className="col-md-6 col-lg-4">
-              <Link
-                href="/services/logistics/"
-                className="d-flex align-items-center white-color">
-                <div className="title title--md title title--medium-weight">
-                  الخدمات اللوجستية
-                </div>
-              </Link>
-            </div>
-            <div className="col-md-6 col-lg-4">
-              <Link
-                href="/services/channels-to-market/"
-                className="d-flex align-items-center white-color">
-                <div className="title title--md title title--medium-weight">
-                  قنوات التسويق
-                </div>
-              </Link>
-            </div>
-            <div className="col-md-6 col-lg-4">
-              <Link
-                href="/services/maintenance-and-after-sales/"
-                className="d-flex align-items-center white-color">
-                <div className="title title--md title title--medium-weight">
-                  صيانة و بعد البيع
-                </div>
-              </Link>
-            </div>
-            <div className="col-md-6 col-lg-4">
-              <Link
-                href="/services/transformation-management/"
-                className="d-flex align-items-center white-color">
-                <div className="title title--md title title--medium-weight">
-                  إدارة التحول
-                </div>
-              </Link>
-            </div>
-            <div className="col-md-6 col-lg-4">
-              <Link
-                href="/services/digital-and-it/"
-                className="d-flex align-items-center white-color">
-                <div className="title title--md title title--medium-weight">
-                  تكنولوجيا المعلومات
-                </div>
-              </Link>
-            </div>
-            <div className="col-md-6 col-lg-4">
-              <Link
-                href="/services/finance/"
-                className="d-flex align-items-center white-color">
-                <div className="title title--md title title--medium-weight">
-                  الموارد المالية
-                </div>
-              </Link>
-            </div>
-            <div className="col-md-6 col-lg-4">
-              <Link
-                href="/services/sga/"
-                className="d-flex align-items-center white-color">
-                <div className="title title--md title title--medium-weight">
-                  SG&amp;A
-                </div>
-              </Link>
-            </div>
-            <div className="col-md-6 col-lg-4">
-              <Link
-                href="/services/hr/"
-                className="d-flex align-items-center white-color">
-                <div className="title title--md title title--medium-weight">
-                  الموارد البشرية
-                </div>
-              </Link>
-            </div>
+          <div className="col-lg-7 col-md-9 off-screen off-screen--hide off-screen--rotate-up">
+            <p className="intro">
+              نحن نحفر بعمق لتحديد جذور المشاكل المعقدة والتركيز عليها على تحقيق نتائج ملموسة للعملاء، وهذه هي اختيار
+              تحديات العمليات التي نعمل عليها بانتظام.
+            </p>
           </div>
         </div>
-      </section>
-    </>
+        <div className="row link-list off-screen off-screen--hide off-screen--link-list">
+          {services.map((service, index) => (
+            <div className="col-md-6 col-lg-4" key={index}>
+              <Link href={service.href} className="d-flex align-items-center white-color">
+                <div className="title title--md title--medium-weight">{service.title}</div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 };
 
