d0138ebfef78dfa86c45ef4dbadf84ffd697890f
Ahmed Hemdan <ahmedashrafhemdan74@gmail.com>
2024-07-03 20:00:22 +0300

    adding whatsapp

--- app/components/WhatsLink.tsx
+++ app/components/WhatsLink.tsx
@@ -4,13 +4,13 @@ import styles from '../styles/WhatsLink.module.css'
 const WhatsLink = () => {
   return (
     <>
-      {/* <div className={`${styles.mainDiv}`}>
+      <div className={`${styles.mainDiv}`}>
         <a href="https://wa.me/+201028485872">
-           <BsWhatsapp size={30} className={`${styles.iconStyle}`}/>
+          <BsWhatsapp size={30} className={`${styles.iconStyle}`} />
         </a>
         <div className={`tooltip fade bs-tooltip-top show ${styles.secondDiv}`}>
         </div>
-    </div> */}
+      </div>
     </>
   )
 }
