a6e52a1f72ceaec6479117c034fddf9fb6e20698
Ahmed Hemdan <ahmedashrafhemdan74@gmail.com>
2024-07-03 20:02:40 +0300

    update site apis

--- app/lib/fetchServices.ts
+++ app/lib/fetchServices.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { IndustryShort } from '../types';
 
-const API_URL = `${process.env.NEXT_PUBLIC_API_URL}eb987d3c-1a94-43ba-abb2-bd01eaa03791/industries`;
+const API_URL = `${process.env.NEXT_PUBLIC_API_URL}eb987d3c-1a94-43ba-abb2-bd01eaa03791/services`;
 
 export const fetchIndustries = async (): Promise<IndustryShort[]> => {
   const response = await axios.get(API_URL);
