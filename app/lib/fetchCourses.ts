9539bc65cb7a73a256ed40247a7e825b815bb1d6
Ahmed Hemdan <ahmedashrafhemdan74@gmail.com>
2024-07-05 18:54:46 +0300

    update fetch services & courses

--- app/lib/fetchCourses.ts
+++ app/lib/fetchCourses.ts
@@ -3,7 +3,7 @@ import { CourseShort } from '../types';
 
 const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/services`;
 
-export const fetchIndustries = async (): Promise<CourseShort[]> => {
+export const fetchCourses = async (): Promise<CourseShort[]> => {
   const response = await axios.get(API_URL);
   return response.data;
 };
--- app/lib/fetchServices.ts
+++ app/lib/fetchServices.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import { ServiceShort } from '../types';
 
-const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/services`;
+const API_URL = `${process.env.NEXT_PUBLIC_API_URL}services`;
 
-export const fetchIndustries = async (): Promise<ServiceShort[]> => {
+export const fetchServices = async (): Promise<ServiceShort[]> => {
   const response = await axios.get(API_URL);
   return response.data;
 };
