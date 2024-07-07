a5bc37e3bf9a927a8f052dbb8e337729828f0b30
Ahmed Hemdan <ahmedashrafhemdan74@gmail.com>
2024-07-07 21:30:43 +0300

    solving build error

--- app/Context/LanguageContext.tsx
+++ app/Context/LanguageContext.tsx
@@ -1,10 +1,20 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import transScript from '../helper/transScript';
 import indexTrans from '../localization/index.trans';
-const LanguageContext = createContext();
 
-const LanguageProvider = ({ children }) => {
-  const [language, setLanguage] = useState('en');
+interface LanguageContextProps {
+  language: string;
+  toggleLanguage: () => void;
+}
+
+const LanguageContext = createContext<LanguageContextProps | undefined>(undefined);
+
+interface LanguageProviderProps {
+  children: ReactNode;
+}
+
+const LanguageProvider: React.FC<LanguageProviderProps> = ({ children }) => {
+  const [language, setLanguage] = useState<string>('en');
 
   useEffect(() => {
     const storedLanguage = localStorage.getItem('lang');
@@ -29,8 +39,11 @@ const LanguageProvider = ({ children }) => {
   );
 };
 
-const useLanguage = () => {
+const useLanguage = (): LanguageContextProps => {
   const context = useContext(LanguageContext);
+  if (!context) {
+    throw new Error('useLanguage must be used within a LanguageProvider');
+  }
   return context;
 };
 
