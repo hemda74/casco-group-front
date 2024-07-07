a3b40b4125bd55af2df7b3ba8f0731757f21df5b
Ahmed Hemdan <ahmedashrafhemdan74@gmail.com>
2024-07-07 20:53:07 +0300

    update translation module

---
+++ app/localization/index.trans.ts
@@ -0,0 +1,15 @@
+type translataionContainer = {
+  [key: string]: {
+    [key: string]: string;
+  };
+};
+const indexTrans: translataionContainer = {
+  // localization button in login page
+  langs: {
+    en: 'ع',
+    ar: 'E',
+  },
+  //////////////< Titles >////////////////////
+};
+
+export default indexTrans;
