8e5bd8a3e50ccd438c1ed05c7297cb3b36b8ae1b
Ahmed Hemdan <ahmedashrafhemdan74@gmail.com>
2024-07-08 03:05:18 +0300

    fix error

--- app/components/Industries/Sidebar.tsx
+++ app/components/Industries/Sidebar.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Link from 'next/link';
 import { Industry, ServiceShort } from '../../types';
+
 type Props = {
     industry: Industry,
     services: ServiceShort[]
 };
+
 const Sidebar: React.FC<Props> = ({ industry, services }) => {
     return (
         <div className="col-lg-4 offset-lg-1 sidebar">
             {industry.expertIndustry.map((ex, index) => (
-
-                <div className="pad-40 light-grey-background red-1-border-top contact-card">
-                    <div className="row align-items-start" key={index}>
+                <div key={index} className="pad-40 light-grey-background red-1-border-top contact-card">
+                    <div className="row align-items-start">
                         <div className="col-md-12">
                             <h4 className="title title--sm red-1-color">
                                 Contact our {industry.name} experts
@@ -30,24 +31,22 @@ const Sidebar: React.FC<Props> = ({ industry, services }) => {
                             <div>{ex.expert_phone}</div>
                             <p className="no-wrap">
                                 <span className="data-label red-1-color">Email</span>
-                                <a href={`mailto: ${ex.expert_mail}`}>
+                                <a href={`mailto:${ex.expert_mail}`}>
                                     {ex.expert_mail}
                                 </a>
                             </p>
                         </div>
                         <div className="contact-card__profile">
                             <picture className="circle-img circle-img--small">
-                                <img src={`${ex.imageUrl}`} />
+                                <img src={`${ex.imageUrl}`} alt={`${ex.expert_name}`} />
                             </picture>
                         </div>
                     </div>
-
                 </div>
             ))}
             <div className="sidebar-nav red-4-background color-white">
                 <div className="title title--sm title--medium-weight">
-                    Explore our services
-                </div>
+                    Explore Oue Services                </div>
                 <ul>
                     {services.map((service, index) => (
                         <li className="page_item page-item-42 page_item_has_children" key={index}>
@@ -56,7 +55,6 @@ const Sidebar: React.FC<Props> = ({ industry, services }) => {
                             </Link>
                         </li>
                     ))}
-
                 </ul>
             </div>
         </div>
