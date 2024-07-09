21dfc206ee2675721081b8a0999b84c8b0e4d067
Ahmed Hemdan <ahmedashrafhemdan74@gmail.com>
2024-07-09 15:44:53 +0300

    remove old baseurl and make it .env var

--- app/servcies/baseUrl.ts
+++
@@ -1,7 +0,0 @@
-const baseURL = () => {
-    // const url: string = window.location.hostname;
-    const globalUrl: string = 'http://202.182.119.166:8000/';
-    return { globalUrl };
-}
-
-export default baseURL;
