10fd7daa25f1c6205f7e9608509a8b307bd2c9a7
hemda74 <ahmedashrafhemdan74@gmail.com>
2024-09-23 23:49:13 +0300

    ......

--- app/pages/industries/[id].tsx
+++ app/pages/industries/[id].tsx
@@ -1,69 +1,80 @@
-// pages/industries/[id].tsx
-import React, { ReactElement } from 'react';
-import Head from 'next/head';
-import { GetStaticPaths, GetStaticProps } from 'next';
-import { useLanguage } from '../../Context/LanguageContext';
-import styles from '../../styles/Main.module.css';
-import { NextPageWithLayout } from '../_app';
-import ViewerLayout from '../../layouts/ViewerLayout';
-import SingleIndustryEn from '../../components/Industries/SingleIndustryEn';
-import SingleIndustryAr from '../../components/Industries/SingleIndustryAr';
-import { fetchIndustryById, fetchIndustries } from '../../lib/fetchIndustries';
-import { fetchServices } from '../../lib/fetchServices';
-import { Industry, ServiceShort, CaseStudy } from '../../types';
-import { fetchCaseStudiesByIndustryId } from '../../lib/fetchCaseStudies';
+// // pages/industries/[id].tsx
+// import React, { ReactElement } from 'react';
+// import Head from 'next/head';
+// import { GetStaticPaths, GetStaticProps } from 'next';
+// import { useLanguage } from '../../Context/LanguageContext';
+// import styles from '../../styles/Main.module.css';
+// import { NextPageWithLayout } from '../_app';
+// import ViewerLayout from '../../layouts/ViewerLayout';
+// import SingleIndustryEn from '../../components/Industries/SingleIndustryEn';
+// import SingleIndustryAr from '../../components/Industries/SingleIndustryAr';
+// import { fetchIndustryById, fetchIndustries } from '../../lib/fetchIndustries';
+// import { fetchServices } from '../../lib/fetchServices';
+// import { Industry, ServiceShort, CaseStudy } from '../../types';
+// import { fetchCaseStudiesByIndustryId } from '../../lib/fetchCaseStudies';
 
-type Props = {
-  industry: Industry;
-  services: ServiceShort[];
-  caseStudies: CaseStudy[];
-};
+// type Props = {
+//   industry: Industry;
+//   services: ServiceShort[];
+//   caseStudies: CaseStudy[];
+// };
 
-const SingleIndustryPage: NextPageWithLayout<Props> = ({ industry, services, caseStudies }) => {
-  const { language } = useLanguage();
+// const SingleIndustryPage: NextPageWithLayout<Props> = ({ industry, services, caseStudies }) => {
+//   const { language } = useLanguage();
+//   return (
+//     <>
+//       <Head>
+//         <title>{industry.name} | CASCO</title>
+//       </Head>
+//       {language === 'en' ? (
+//         <main className={styles.bodyContainer}>
+//           <SingleIndustryEn industry={industry} services={services} caseStudies={caseStudies} />
+//         </main>
+//       ) : (
+//         <main className={styles.bodyContainer}>
+//           <SingleIndustryAr industry={industry} services={services} caseStudies={caseStudies} />
+//         </main>
+//       )}
+//     </>
+//   );
+// };
+
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const industries = await fetchIndustries();
+//   const paths = industries.map((industry) => ({
+//     params: { id: industry.id },
+//   }));
+//   return { paths, fallback: false };
+// };
+
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   const { id } = params as { id: string };
+//   const industry = await fetchIndustryById(id);
+//   const services = await fetchServices();
+//   const caseStudies = await fetchCaseStudiesByIndustryId(id);
+//   return {
+//     props: {
+//       industry,
+//       services,
+//       caseStudies,
+//     },
+//   };
+// };
+// // adding Layout
+// SingleIndustryPage.getLayout = function getLayout(page: ReactElement) {
+//   return <ViewerLayout childern={page}></ViewerLayout>;
+// };
+
+// export default SingleIndustryPage;
+// // pages/industries/[id].tsx
+import React from 'react'
+
+const id = () => {
   return (
-    <>
-      <Head>
-        <title>{industry.name} | CASCO</title>
-      </Head>
-      {language === 'en' ? (
-        <main className={styles.bodyContainer}>
-          <SingleIndustryEn industry={industry} services={services} caseStudies={caseStudies} />
-        </main>
-      ) : (
-        <main className={styles.bodyContainer}>
-          <SingleIndustryAr industry={industry} services={services} caseStudies={caseStudies} />
-        </main>
-      )}
-    </>
-  );
-};
+    <div>
+      hello
+    </div>
+  )
+}
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const industries = await fetchIndustries();
-  const paths = industries.map((industry) => ({
-    params: { id: industry.id },
-  }));
-  return { paths, fallback: false };
-};
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { id } = params as { id: string };
-  const industry = await fetchIndustryById(id);
-  const services = await fetchServices();
-  const caseStudies = await fetchCaseStudiesByIndustryId(id);
-  return {
-    props: {
-      industry,
-      services,
-      caseStudies,
-    },
-  };
-};
-// adding Layout
-SingleIndustryPage.getLayout = function getLayout(page: ReactElement) {
-  return <ViewerLayout childern={page}></ViewerLayout>;
-};
-
-export default SingleIndustryPage;
-// pages/industries/[id].tsx
+export default id
--- app/pages/services/[id].tsx
+++ app/pages/services/[id].tsx
@@ -1,64 +1,75 @@
-// pages/services/[id].tsx
-import React, { ReactElement } from 'react';
-import Head from 'next/head';
-import { GetStaticPaths, GetStaticProps } from 'next';
-import { useLanguage } from '../../Context/LanguageContext';
-import styles from '../../styles/Main.module.css';
-import { NextPageWithLayout } from '../_app';
-import ViewerLayout from '../../layouts/ViewerLayout';
-import { fetchServiceById, fetchServices } from '../../lib/fetchServices';
-import { Service, ServiceShort, CaseStudy } from '../../types';
-import ServiceAr from '../../components/Services/ServiceAr';
-import ServiceEn from '../../components/Services/ServiceEn';
+// // pages/services/[id].tsx
+// import React, { ReactElement } from 'react';
+// import Head from 'next/head';
+// import { GetStaticPaths, GetStaticProps } from 'next';
+// import { useLanguage } from '../../Context/LanguageContext';
+// import styles from '../../styles/Main.module.css';
+// import { NextPageWithLayout } from '../_app';
+// import ViewerLayout from '../../layouts/ViewerLayout';
+// import { fetchServiceById, fetchServices } from '../../lib/fetchServices';
+// import { Service, ServiceShort, CaseStudy } from '../../types';
+// import ServiceAr from '../../components/Services/ServiceAr';
+// import ServiceEn from '../../components/Services/ServiceEn';
 
-type Props = {
-  service: Service;
-  services: ServiceShort[]
-};
+// type Props = {
+//   service: Service;
+//   services: ServiceShort[]
+// };
 
-const SingleservicePage: NextPageWithLayout<Props> = ({ service, services }) => {
-  const { language } = useLanguage();
+// const SingleservicePage: NextPageWithLayout<Props> = ({ service, services }) => {
+//   const { language } = useLanguage();
+//   return (
+//     <>
+//       <Head>
+//         <title>{service.name} | CASCO</title>
+//       </Head>
+//       {language === 'en' ? (
+//         <main className={styles.bodyContainer}>
+//           <ServiceEn service={service} services={services} />
+//         </main>
+//       ) : (
+//         <main className={styles.bodyContainer}>
+//           <ServiceAr service={service} services={services} />
+//         </main>
+//       )}
+//     </>
+//   );
+// };
+
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const services = await fetchServices();
+//   const paths = services.map((service) => ({
+//     params: { id: service.id },
+//   }));
+//   return { paths, fallback: false };
+// };
+
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   const { id } = params as { id: string };
+//   const service = await fetchServiceById(id);
+//   const services = await fetchServices();
+//   return {
+//     props: {
+//       service,
+//       services,
+//     },
+//   };
+// };
+// // adding Layout
+// SingleservicePage.getLayout = function getLayout(page: ReactElement) {
+//   return <ViewerLayout childern={page}></ViewerLayout>;
+// };
+
+// export default SingleservicePage;
+// // pages/services/[id].tsx
+import React from 'react'
+
+const id = () => {
   return (
-    <>
-      <Head>
-        <title>{service.name} | CASCO</title>
-      </Head>
-      {language === 'en' ? (
-        <main className={styles.bodyContainer}>
-          <ServiceEn service={service} services={services} />
-        </main>
-      ) : (
-        <main className={styles.bodyContainer}>
-          <ServiceAr service={service} services={services} />
-        </main>
-      )}
-    </>
-  );
-};
+    <div>
+      hello
+    </div>
+  )
+}
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const services = await fetchServices();
-  const paths = services.map((service) => ({
-    params: { id: service.id },
-  }));
-  return { paths, fallback: false };
-};
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { id } = params as { id: string };
-  const service = await fetchServiceById(id);
-  const services = await fetchServices();
-  return {
-    props: {
-      service,
-      services,
-    },
-  };
-};
-// adding Layout
-SingleservicePage.getLayout = function getLayout(page: ReactElement) {
-  return <ViewerLayout childern={page}></ViewerLayout>;
-};
-
-export default SingleservicePage;
-// pages/services/[id].tsx
+export default id
