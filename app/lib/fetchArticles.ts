c84cebc6ba889b21f0717caa370f5021c856cc11
hemda74 <ahmedashrafhemdan74@gmail.com>
2024-09-24 00:11:51 +0300

    .

--- app/lib/fetchArticles.ts
+++ app/lib/fetchArticles.ts
@@ -1,15 +1,15 @@
-// import axios from 'axios';
-// import { Event3 } from '../types';
+import axios from 'axios';
+import { Event3 } from '../types';
 
-// const API_URL =
-//   `${process.env.NEXT_PUBLIC_API_URL}articles` || 'http://127.0.0.1:80';
+const API_URL =
+  `${process.env.NEXT_PUBLIC_API_URL}articles` || 'http://127.0.0.1:80';
 
-// export const fetchArticles = async (): Promise<Event3[]> => {
-//   const response = await axios.get(API_URL);
-//   return response.data;
-// };
+export const fetchArticles = async (): Promise<Event3[]> => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
 
-// export const fetchArticleById = async (id: string): Promise<Event3> => {
-//   const response = await axios.get(`${API_URL}/${id}`);
-//   return response.data;
-// };
+export const fetchArticleById = async (id: string): Promise<Event3> => {
+  const response = await axios.get(`${API_URL}/${id}`);
+  return response.data;
+};
